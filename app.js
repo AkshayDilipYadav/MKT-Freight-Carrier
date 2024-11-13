e8e2fe3e47c4c27e74282c7cffffef527b9533f5
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-11-14 04:38:06 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 let items = document.querySelectorAll('.allowActive');
-let containerFuild = document.querySelector('.container-fuild');
 
-containerFuild.addEventListener('scroll', ()=>{
-    items.forEach(item => {
-        if(item.offsetTop - containerFuild.scrollTop < window.innerHeight*(3/4)){
-            item.classList.add('active');
-        }else{
-            item.classList.remove('active');
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        } else {
+            entry.target.classList.remove('active');
         }
-    })
-})
+    });
+}, { threshold: 0.75 }); // 75% of the item should be in view
+
+items.forEach(item => observer.observe(item));
+
+
+
+let valueDisplays = document.querySelectorAll(".num1");
+let interval = 5000;
+
+valueDisplays.forEach((valueDisplay) => {
+let startValue = 0;
+let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+let duration = Math.floor(interval / endValue);
+let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue.toString().padStart(3, '0');
+    if(startValue == endValue){
+        clearInterval(counter);
+    }
+}, duration);
+});
